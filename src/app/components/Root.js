486f4824fdf3e99fda4e7ff54b313c392baba275
Julia <julia.avsyuk@mail.ru>
2018-01-05 13:22:36 +0300

    Root.js

--- src/app/components/Root.js
+++ src/app/components/Root.js
@@ -1,3 +1,22 @@
-/**
- * Created by ЮЛЯ on 14.10.2017.
- */
+import React from "react";
+import {Header} from "./Header";
+
+export class Root extends React.Component {
+    render () {
+        return(
+            <div className="container">
+                <div className="row">
+                    <div className="col-xs-10 col-xs-offset-1">
+                        <Header />
+                    </div>
+                </div>
+                <hr />
+                <div className="row">
+                    <div className="col-xs-10 col-xs-offset-1">
+                        {this.props.children}
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
