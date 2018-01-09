6ffb827b3a126f9ce82a4770c29d6d0c056f46bd
Julia <julia.avsyuk@mail.ru>
2018-01-09 17:29:17 +0300

    add old project

---
+++ src/app/index.js
@@ -0,0 +1,27 @@
+import React from  "react";
+import { render } from "react-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+import { Root } from "./components/Root";
+import { Home } from "./components/Home";
+import { User } from "./components/User";
+
+class App extends React.Component {
+    render() {
+        return (
+            <Router>
+                <div>
+                    <Root>
+                        <Switch>
+                            <Route exact path="/" component={Home} />
+                            <Route path="/user/:id" component={User} />
+                            <Route path="/home" component={Home} />
+                        </Switch>
+                    </Root>
+                </div>
+            </Router>
+        )
+    }
+}
+
+render(<App/>, window.document.getElementById("app"));
